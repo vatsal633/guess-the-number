5d9dd2f861f07d53f302f72fb1e50cdb060415ab
vatsal2004 <153715547+vatsal633@users.noreply.github.com>
2024-03-17 21:30:17 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,34 +1,38 @@
 let ran_num = Math.floor(Math.random() * 100 + 1)
-let gues = 1;
+let gues = 0;
 console.log(ran_num)
 
 function check() {
     let user_number = document.getElementById('inp').value
 
     if (user_number == ran_num) {
-        document.getElementById('ans').innerHTML = "you guess it right"
-        document.getElementById('trys').innerHTML = "total guess: " + gues;
-        let score = document.getElementById('score')
+        document.getElementById('ans').innerHTML = "You Guess It Right"
+        document.getElementById('ans').style.color = 'green'
+        document.getElementById('ans').style.fontWeight = '600'
+        document.getElementById('ans').style.fontSize = '23px'
+
+        document.getElementById('trys').innerHTML = "Total Guess: " + gues;
+        let score = document.getElementById('score')                        
 
         let final_score = 100 - gues
 
-        score.innerHTML = "score " + final_score
+        score.innerHTML = "Your Score Is: " + final_score
 
-        let re_button = document.getElementById('btn').innerHTML = "<button>Restart the game</button>"
+        let re_button = document.getElementById('btn').innerHTML = "<button>Restart The Game</button>"
 
     }
 
     else if(user_number>100){
-        document.getElementById('ans').innerHTML = "pls enter number between 1 to 100"
+        document.getElementById('ans').innerHTML = "Pls Enter Number Between 1 to 100"
     }
 
     else if (user_number > ran_num) {
-        document.getElementById('ans').innerHTML = "your num is greater than actual number"
+        document.getElementById('ans').innerHTML = "Your Num Is Greater Than Actual Number"
         gues++;
     }
 
     else {
-        document.getElementById('ans').innerHTML = "your num is less than actual number"
+        document.getElementById('ans').innerHTML = "Your Num Is Less Than Actual Number"
         gues++;
     }
 
@@ -40,7 +44,3 @@ document.getElementById('btn').onclick = function restart(){
     location.reload();
     
 }
-
-
-
-
